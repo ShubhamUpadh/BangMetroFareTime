be2ca097c62830f1b88327eebf292a3edb2a90fb
ShubhamUpad <01shubhamupadhyay@gmail.com>
2023-01-18 23:12:35 +0530

    Fare Calculator

--- stations.py
+++ stations.py
@@ -1,6 +1,8 @@
 import networkx as nx
 import matplotlib.pylab as plt
 
+pathLength = 0
+
 
 class Graph:
     def __init__(self, edges):
@@ -41,6 +43,61 @@ class Graph:
             count += 1
             print(f"Station {count} = {key} is connected with = {self.graph_dict[key]}", end=" \n")
         # print(self.graph_dict)
+
+    def bfsPath(self, start, goal):
+
+        explored = []
+
+        # Queue for traversing the
+        # graph in the BFS
+        queue = [[start]]
+
+        # If the desired node is
+        # reached
+        if start == goal:
+            print("Same Node")
+            return 1
+
+        # Loop to traverse the graph
+        # with the help of the queue
+        while queue:
+            path = queue.pop(0)
+            node = path[-1]
+
+            # Condition to check if the
+            # current node is not visited
+            if node not in explored:
+                neighbours = self.graph_dict[node]
+
+                # Loop to iterate over the
+                # neighbours of the node
+                for neighbour in neighbours:
+                    new_path = list(path)
+                    new_path.append(neighbour)
+                    queue.append(new_path)
+                    global pathLength
+                    pathLength = pathLength + 1
+                    # Condition to check if the
+                    # neighbour node is the goal
+                    if neighbour == goal:
+                        print("Shortest path = ", *new_path)
+                        return len(new_path)
+                explored.append(node)
+
+        # Condition when the nodes
+        # are not connected
+        print("So sorry, but a connecting" \
+              "path doesn't exist :(")
+        return len(new_path)
+
+    def fareCalc(self, numstations):
+        numstations = numstations -1
+        stationsTravelled = {
+            0: 10, 1: 10, 2: 15, 3: 15, 4: 18, 5: 20, 6: 22, 7: 25, 8: 28, 9: 30, 10: 30, 11: 35, 12: 35, 13: 38,
+            14: 40, 15: 42, 16: 45, 17: 45, 18: 50, 19: 50, 20: 52, 21: 55, 22: 58, 23: 60, 24: 60
+        }
+        return stationsTravelled[numstations]
+
 
 
 if __name__ == "__main__":
@@ -83,12 +140,16 @@ if __name__ == "__main__":
     ]
 
     metroRoute = Graph(routes)
-
+    # metroRoute.printEdges()
+    # print(routeStations,"\n",len(routeStations))
+    k = metroRoute.bfsPath("Peenya Industry", "Peenya")
+    print(metroRoute.fareCalc(k))
+'''
     G = nx.Graph(metroRoute.graph_dict)
     pos = nx.spring_layout(G)
     nx.draw_networkx_nodes(G, pos)
     nx.draw_networkx_edges(G, pos,connectionstyle="Bar")
     nx.draw_networkx_labels(G, pos,font_size=5)
     plt.show()
-
-    metroRoute.printEdges()
+'''
+# metroRoute.printEdges()
